9cb9e6f6d524126a74f429db3cc69fb2bf40c801
Nikolay Kim <nuss@yandex.ru>
2022-10-10 23:51:04 +0300

    minor changes in README and output in run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@ echo "creating a 10MB sample file"
 dd if=/dev/zero of=/tmp/file.dd count=10 bs=1M
 echo "the sample has been created "
 
-echo "start to copy with class 3"
+echo "start to copy with class 3 (idle)"
 (ionice -c 3 dd if=/tmp/file.dd of=/tmp/file3.dd iflag=direct && echo "ionice with class 3 (idle) finished" && echo ====================================) &
-echo "start to copy with class 1 priority 0"
+echo "start to copy with class 1 (realtime) priority 0"
 (ionice -c 1 -n 0 dd if=/tmp/file.dd of=/tmp/file1-0.dd iflag=direct && echo "ionice with class 1 (realtime) priority 0 finished" && echo ====================================) &
-echo "start to copy with class 1 priority 7"
+echo "start to copy with class 1 (realtime) priority 7"
 (ionice -c 1 -n 7 dd if=/tmp/file.dd of=/tmp/file1-7.dd iflag=direct && echo "ionice with class 1 (realtime) priority 7 finished" && echo ====================================) &
-echo "start to copy with class 2 priority 0"
+echo "start to copy with class 2 (best effort - standard) priority 0"
 (ionice -c 2 -n 0 dd if=/tmp/file.dd of=/tmp/file1-7.dd iflag=direct && echo "ionice with class 2 (best effort) priority 0 finished" && echo ====================================) &
 
 ps ax | grep iflag
